9748ce99462295293e1ecf9a8adbcd1cb4c574d8
ecaoili24 <erika.caoili@gmail.com>
2020-04-02 23:53:47 +0000

    Initial commit of the state file

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+import os
 
 
 class State(BaseModel):
@@ -8,4 +10,16 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade"delete")
+
+    else:
+        @property
+        def cities(self):
+            city_LIST = []
+            for obj in models.storage.all(City).values():
+                city_LIST.append(obj)
+            return city_LIST
