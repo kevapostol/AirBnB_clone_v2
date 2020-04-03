7c40b08198fe2c7790ff822f59526274ed72c724
kevapostol <apostolkevin@gmail.com>
2020-04-03 05:27:58 +0000

    changed backref

--- models/user.py
+++ models/user.py
@@ -20,4 +20,4 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
 
     places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="place", cascade="all, delete")
