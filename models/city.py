aa5cd6e7eea2846991a74ad17fcf2c68173fadb6
kevapostol <apostolkevin@gmail.com>
2020-04-03 00:20:04 +0000

    fixed spelling of backref

--- models/city.py
+++ models/city.py
@@ -15,4 +15,4 @@ class City(BaseModel):
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
 
-    places = relationship("Place", bacref="cities", cascade="delete")
+    places = relationship("Place", backref="cities", cascade="delete")
