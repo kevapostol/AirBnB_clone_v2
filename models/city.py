b74083c1fd92d7b5938542977f0e615aaf0c8f52
kevapostol <apostolkevin@gmail.com>
2020-04-03 02:03:33 +0000

    Added Base to inherit

--- models/city.py
+++ models/city.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
