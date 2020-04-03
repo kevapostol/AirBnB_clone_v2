5ec20c3d2c6584951ade48294fdb64f75c63a50e
ACholberton <1103@holbertonschool.com>
2020-04-03 00:36:16 +0000

    made changes and added more tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -2,6 +2,7 @@
 """Database storage testing"""
 import unittest
 import os
+import MySQLdb
 import models.engine.db_storage
 from models.state import State
 from models import storage
@@ -11,4 +12,17 @@ from models import storage
                  "Using filesystem isntead of database")
 class TestDBStorage(unittest.TestCase):
     """ Tests the database storage"""
-    def test_new(self):
+    def setUp(self):
+        self.db = MySQLdb.connect(host="localhost", port=3306,
+                                  user="HBNB_MYSQL_USER",
+                                  passwd="HBNB_MYSQL_PWD",
+                                  db="HBNB_MYSQL_DB")
+        self.cur = self.db.cursor()
+        self.storage = DBStorage()
+        self.storage.reload()
+
+
+    def tearDown(self):
+        """ close the connection"""
+        self.cur.close
+        self.db.close()
