3f425f35294c4ddd7d7704a1fe1b715f8cd075cf
kevapostol <apostolkevin@gmail.com>
2020-04-06 21:36:53 +0000

    setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Task 0 Configuration
+
+# NGINX Installation
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/releases/test/
+
+# Create a fake HTML file /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+# -s is create a symbolic link
+# -f is if the symbolic link already exists, it should be deleted and recreated every time the script is ran.
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+# -R is recursive
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ {alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+
+# Restart NGINX
+sudo service nginx restart
