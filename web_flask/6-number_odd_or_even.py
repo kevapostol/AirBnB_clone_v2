bcdd8dd9ced34d1449875a441ef4960b51765431
kevapostol <apostolkevin@gmail.com>
2020-04-20 23:24:43 +0000

    Odd or even routing by using a template (jinja and flask)

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+'''
+Module: Basic Flask functionality
+'''
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    '''
+    rtype: str
+    '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def displayHBNB():
+    '''
+    rtype: str
+    '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    '''
+    rtype: str
+    '''
+    return text.replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text):
+    '''
+    rtype: str
+    '''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+def pythonIsCool():
+    '''
+    rtype: str
+    '''
+    return "Python is cool"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    '''
+    rtype: str
+    '''
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_temp(n):
+    '''
+    rtype: render_template
+    '''
+    return render_template("5-number.html", num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def odd_or_even(n):
+    '''
+    rtype: render_template
+    '''
+    return render_template("6-number_odd_or_even.html", num=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
