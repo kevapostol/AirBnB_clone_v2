3b2c076ef6e1dd7c631ff2a24948f766432bba78
kevapostol <apostolkevin@gmail.com>
2020-04-20 21:54:04 +0000

    Added python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+'''
+Module: Basic Flask functionality
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    '''
+    rtype: str
+    '''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def displayHBNB():
+    '''
+    rtype: str
+    '''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    '''
+    rtype: str
+    '''
+    return text.replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text):
+    '''
+    rtype: str
+    '''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+def pythonIsCool():
+    '''
+    rtype: str
+    '''
+    return "Python is cool"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
