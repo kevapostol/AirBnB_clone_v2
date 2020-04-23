a89b5d2a6a023bb16d2bbc557e2ff8434336e8b9
kevapostol <apostolkevin@gmail.com>
2020-04-23 01:30:41 +0000

    Outputs Cities by states using flask

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A python script that uses flask
+"""
+
+from flask import Flask, render_template
+from models import storage, State
+from flask import escape
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def show_cities_by_state():
+    '''
+    rtype: Dictionary: of States
+    '''
+    state_dict = storage.all(State)
+    return render_template('8-cities_by_states.html', state_dict=state_dict)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    '''
+    Removes the current SQLAlchemy Session
+    '''
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
